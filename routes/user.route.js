d72d6c0df2219c6951b0f52abf7b04fa967300ac
sagar ninave <sagarninave@gmail.com>
2021-02-06 19:57:30 +0530

    upload profile image code saperated

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,46 +4,7 @@ var router = express.Router();
 const userController = require('../controller/user.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
-const fs = require('fs');
-const multer = require('multer');
-const path = require('path');
-
-// profile image upload start //
-let storagepath = './storage';
-let imagepath = storagepath + '/images';
-let profileimagepath = imagepath + '/profile';
-let folderpath = [storagepath, imagepath, profileimagepath];
-function createfolders(){
-  folderpath.map(fp => {
-    if (!fs.existsSync(fp)) {
-      fs.mkdirSync(fp);
-    }
-  })
-}
-const profilepicturestorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    createfolders();
-    cb(null, profileimagepath);
-  },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
-  }
-});
-const pps = multer({ 
-  storage: profilepicturestorage,
-  limits : {
-    fileSize: 1024* 1024 * 5
-  },
-  fileFilter : function(req, file, cb){
-    if(file.mimetype == "image/jpeg" || file.mimetype == "image/png"){
-      cb(null, true);
-    }
-    else{
-      cb(null, false);
-    }
-  }
-});
-// profile image upload start //
+const {pps} = require('../functions/uploadprofilepicture');
 
 router.get("/checkuserexists/:email", userController.checkuserexists);
 router.post("/signup", userController.signup);
