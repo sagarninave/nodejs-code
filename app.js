3c30b501c7ed14c1ceb6d41f26273e87c24e88eb
sagar ninave <sagarninave@gmail.com>
2021-02-06 19:41:49 +0530

    uploaded profile image

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.options('*', cors());
 app.use(cors())
+app.use(express.static('./storage/'));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,47 @@ var router = express.Router();
 const userController = require('../controller/user.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+// profile image upload start //
+let storagepath = './storage';
+let imagepath = storagepath + '/images';
+let profileimagepath = imagepath + '/profile';
+let folderpath = [storagepath, imagepath, profileimagepath];
+function createfolders(){
+  folderpath.map(fp => {
+    if (!fs.existsSync(fp)) {
+      fs.mkdirSync(fp);
+    }
+  })
+}
+const profilepicturestorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    createfolders();
+    cb(null, profileimagepath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+  }
+});
+const pps = multer({ 
+  storage: profilepicturestorage,
+  limits : {
+    fileSize: 1024* 1024 * 5
+  },
+  fileFilter : function(req, file, cb){
+    if(file.mimetype == "image/jpeg" || file.mimetype == "image/png"){
+      cb(null, true);
+    }
+    else{
+      cb(null, false);
+    }
+  }
+});
+// profile image upload start //
+
 router.get("/checkuserexists/:email", userController.checkuserexists);
 router.post("/signup", userController.signup);
 router.get("/verifyemail/:userId", userController.verifyemail);
@@ -15,6 +56,7 @@ router.get("/recentloginemailsend", userController.recentloginemailsend);
 router.get("/getalluser", authMiddleware, userController.getalluser);
 router.get("/getuser/:id", authMiddleware, userController.getuser);
 router.get("/userprofile", authMiddleware, userController.userprofile);
+router.post("/uploadprofilepicture", pps.single("profilepicture"), userController.uploadprofilepicture);
 router.put("/edituserprofile", authMiddleware, userController.edituserprofile);
 
 module.exports = router;
