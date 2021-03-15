848a0c7591c9d52fdc435d60332c4fc2bd34649e
sagar ninave <sagarninave@gmail.com>
2021-03-15 12:46:57 +0530

    deployment

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const port =  process.env.port || 8000;
 const http = require("http");
 const app = require('./app'); 
 const server = http.createServer(app);
-server.listen(port, ()=>{console.log(`server running on port http://localhost:${port}`)});
-  
+server.listen(port, () => {
+  console.log(`server running on port http://localhost:${port}`)
+});
