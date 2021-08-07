9ba7637d7fff617c33a6aab72a067c7c7b3ffa57
sagar ninave <sagarninave@gmail.com>
2021-08-08 00:16:24 +0530

    src folder created

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const port =  process.env.port || 8000;
 const http = require("http");
-const app = require('./app'); 
+const app = require('./src/app'); 
 const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`server running on port http://localhost:${port}`)
