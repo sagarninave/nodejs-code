67cb378248d072da9b8933ccf75389d52e73cfe4
Sagar Ninave <sagar.ninave@konverge.ai>
2022-12-15 17:28:58 +0530

    added comment in app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 
+/* This is to configure the cloudinary API. */
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -12,16 +13,21 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRETE
 })
 
+/* A middleware that logs all the requests to the console. */
 app.use(morgan('dev'));
 
+/* Parsing the body of the request. */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/* This is to allow cross origin requests. */
 app.options('*', cors());
 app.use(cors())
 
+/* This is to serve static files. */
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
+/* This is to allow cross origin requests. */
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token");
@@ -32,10 +38,12 @@ app.use((req, res, next) => {
   next();
 });
 
+/* This is to enable swagger documentation for the API. */
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger.json');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+/* This is importing the routes. */
 const { dbbackup } = require('./config/mongoDB');
 const userRoute = require('./routes/user.route');
 
@@ -46,12 +54,14 @@ app.get('/', (req, res) => {
 app.use('/dbbackup', dbbackup);
 app.use('/api/user', userRoute);
 
+/* This is to handle invalid endpoints. */
 app.use((req, res, next) => {
   const error = new Error('Invalid endpoint');
   error.status = 404;
   next(error);
 })
 
+/* This is to handle errors. */
 app.use((error, req, res, next) => {
   let errorResponse = {
     status: 'failed',
